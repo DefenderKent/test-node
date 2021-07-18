403cafed76a3f1c02123e9b5fe9ff2bc86a97244
ikondr <ikondratev@zennex.ru>
2021-07-18 18:27:01 +0700

    add env, add db user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [],
   providers: [],
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'kesha1995kent',
-      database: 'test-app',
-      models: [],
+      host: process.env.POSTGRESS_HOST,
+      port: Number(process.env.POSTGRESS_PORT),
+      username: process.env.POSTGRESS_USER,
+      password: process.env.POSTGRESS_PASSWORD,
+      database: process.env.POSTGRESS_DB,
+      models: [User],
       autoLoadModels: true,
     }),
     UsersModule,
